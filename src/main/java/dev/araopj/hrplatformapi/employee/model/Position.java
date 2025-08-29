ff92ad2f2d0e9e4d49b2a74e6fad9e92a344e4d2
Peter John Arao <pitzzahh@gmail.com>
2025-08-30 00:16:10 +0800

    feat: add Position entity for employee position management

---
+++ src/main/java/dev/araopj/hrplatformapi/employee/model/Position.java
@@ -0,0 +1,36 @@
+package dev.araopj.hrplatformapi.employee.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity(name = "positions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Position {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    String id;
+
+    @Column(nullable = false, unique = true)
+    String code;
+
+    @Column(nullable = false, unique = true)
+    String description;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @Column(nullable = false, updatable = false)
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
