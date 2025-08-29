ecfe667c21d4680b3ce31e755356eed188363fc5
Peter John Arao <pitzzahh@gmail.com>
2025-08-29 19:44:08 +0800

    init: springboot project

---
+++ src/main/java/com/araopeterj/hrcore/HrCoreApplication.java
@@ -0,0 +1,13 @@
+package com.araopeterj.hrcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/araopeterj/hrcore/HrCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araopeterj.hrcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
