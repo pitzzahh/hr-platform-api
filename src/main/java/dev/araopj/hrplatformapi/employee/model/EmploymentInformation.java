def03927dbf1cfbb59772468ade0ea880eafbd87
Peter John Arao <pitzzahh@gmail.com>
2025-08-30 00:18:21 +0800

    feat: add EmploymentInformation and EmploymentInformationSalaryOverride entities for employee employment details management

---
+++ src/main/java/dev/araopj/hrplatformapi/employee/model/EmploymentInformation.java
@@ -0,0 +1,71 @@
+package dev.araopj.hrplatformapi.employee.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "employment_information")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class EmploymentInformation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    String id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    Employee employee;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    EmploymentStatus employmentStatus;
+
+    @Column(nullable = false)
+    String sourceOfFund;
+
+    @Column
+    String remarks;
+
+    @ManyToOne
+    @JoinColumn(name = "employment_information_salary_override_id")
+    EmploymentInformationSalaryOverride salaryOverride;
+
+    @Column(nullable = false)
+    @Size(min = 1, max = 8, message = "Salary step must be between 1 and 8")
+    byte step;
+
+    @Column
+    @Size(min = 1, max = 8, message = "Anticipated step must be between 1 and 8")
+    byte anticipatedStep;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "position_id")
+    Position position;
+
+    @ManyToOne
+    @JoinColumn(name = "station_org_id")
+    DivisionStationPlaceOfAssignment divisionStationPlaceOfAssignment;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @Column(nullable = false, updatable = false)
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
