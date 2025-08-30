45fd192fc7827190f8ad07a0d5695747caeef5b3
Peter John Arao <pitzzahh@gmail.com>
2025-08-31 01:06:36 +0800

    feat: replace @Size with @Min and @Max for salaryGrade validation in SalaryGrade

--- src/main/java/dev/araopj/hrplatformapi/salary/model/SalaryGrade.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/model/SalaryGrade.java
@@ -3,7 +3,8 @@ package dev.araopj.hrplatformapi.salary.model;
 import dev.araopj.hrplatformapi.utils.EntityTimestamp;
 import dev.araopj.hrplatformapi.utils.Uuid;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 import java.io.Serializable;
@@ -32,7 +33,8 @@ public class SalaryGrade extends EntityTimestamp implements Serializable {
     LocalDate effectiveDate;
 
     @Column(nullable = false)
-    @Size(min = 1, max = 33, message = "Salary grade must be between 1 and 33")
+    @Min(value = 1, message = "Salary grade must be at least 1")
+    @Max(value = 33, message = "Salary grade must be at most 33")
     byte salaryGrade;
 
     @OneToMany(mappedBy = "salaryGrade", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
