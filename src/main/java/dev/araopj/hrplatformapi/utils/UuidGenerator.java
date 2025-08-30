5b03a5f84ad2cd6a6718ad7e1a2e00949b688da4
Peter John Arao <pitzzahh@gmail.com>
2025-08-30 21:28:02 +0800

    feat: add UuidGenerator for custom UUID generation with support for client-assigned IDs

---
+++ src/main/java/dev/araopj/hrplatformapi/utils/UuidGenerator.java
@@ -0,0 +1,39 @@
+package dev.araopj.hrplatformapi.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.enhanced.SequenceStyleGenerator;
+
+import java.util.UUID;
+
+/**
+ * Custom UUID generator that allows for client-assigned IDs.
+ * If no ID is assigned, it generates a new UUID.
+ * Assumes the entity has a getId() method to check for existing IDs.
+ * This generator is used with the @Uuid annotation on entity properties to indicate UUID generation strategy.
+ */
+@Slf4j
+public class UuidGenerator extends SequenceStyleGenerator {
+    @Override
+    public Object generate(SharedSessionContractImplementor session, Object owner) {
+        log.debug("Creating UUID if none is assigned");
+        try {
+            if (this.allowAssignedIdentifiers()) {
+                var getIdMethod = owner.getClass().getMethod("getId"); // Assumes the entity has a getId() method
+                Object existingId = getIdMethod.invoke(owner);
+                if (existingId != null) {
+                    return existingId;
+                }
+            }
+        } catch (Exception e) {
+            log.warn("Could not access getId method on owner: {}", e.getMessage());
+        }
+        log.info("Generating new UUID, as none was assigned");
+        return UUID.randomUUID();
+    }
+
+    @Override
+    public boolean allowAssignedIdentifiers() {
+        return true; // REQUIRED to allow client-assigned IDs
+    }
+}
