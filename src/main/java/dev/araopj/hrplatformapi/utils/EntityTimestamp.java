92d3ab0f77714ac2bc3dd9fd4daeb5e1fdc24ebc
Peter John Arao <pitzzahh@gmail.com>
2025-08-30 12:36:32 +0800

    feat: add EntityTimestamp base class for automatic created and updated timestamps

---
+++ src/main/java/dev/araopj/hrplatformapi/utils/EntityTimestamp.java
@@ -0,0 +1,23 @@
+package dev.araopj.hrplatformapi.utils;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class EntityTimestamp {
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
+
