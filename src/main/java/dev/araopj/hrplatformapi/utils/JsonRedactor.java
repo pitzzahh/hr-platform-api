b81980b24ef56100efd03dab6047c7b0a53bcc07
Peter John Arao <pitzzahh@gmail.com>
2025-09-01 00:15:31 +0800

    feat: add JsonRedactor utility for redacting sensitive fields in JSON objects

---
+++ src/main/java/dev/araopj/hrplatformapi/utils/JsonRedactor.java
@@ -0,0 +1,48 @@
+package dev.araopj.hrplatformapi.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.experimental.UtilityClass;
+
+import java.util.Set;
+
+@UtilityClass
+public class JsonRedactor {
+
+    private static final String REDACTED_VALUE = "****";
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * Redacts specified fields from the given object and returns a JsonNode.
+     *
+     * @param data           The input object (DTO, entity, map, etc.).
+     * @param fieldsToRedact The set of field names to redact.
+     * @return A JsonNode with sensitive fields redacted.
+     */
+    public static JsonNode redact(Object data, Set<String> fieldsToRedact) {
+        return redactNode(mapper.valueToTree(data), fieldsToRedact);
+    }
+
+    private static JsonNode redactNode(JsonNode node, Set<String> fieldsToRedact) {
+        if (node.isObject()) {
+            var objectNode = (ObjectNode) node;
+            for (var entry : objectNode.properties()) {
+                var fieldName = entry.getKey();
+                var child = entry.getValue();
+
+                if (fieldsToRedact.contains(fieldName)) {
+                    objectNode.put(fieldName, REDACTED_VALUE);
+                } else {
+                    redactNode(child, fieldsToRedact);
+                }
+            }
+        } else if (node.isArray()) {
+            for (var child : node) {
+                redactNode(child, fieldsToRedact);
+            }
+        }
+        return node;
+    }
+
+}
