4fbb542796c08c2b771d8616315a7742291e14fc
Peter John Arao <pitzzahh@gmail.com>
2025-08-31 16:05:50 +0800

    feat: add method to find salary data by salary grade with limit in repository

--- src/main/java/dev/araopj/hrplatformapi/salary/repository/SalaryDataRepository.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/repository/SalaryDataRepository.java
@@ -1,9 +1,13 @@
 package dev.araopj.hrplatformapi.salary.repository;
 
 import dev.araopj.hrplatformapi.salary.model.SalaryData;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SalaryDataRepository extends JpaRepository<SalaryData,String> {
+public interface SalaryDataRepository extends JpaRepository<SalaryData, String> {
+    List<SalaryData> findBySalaryGrade_Id(String salaryGradeId, Limit limit);
 }
