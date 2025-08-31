3b7bf0d10565784acd34c14a51eb8f14ebc30a91
Peter John Arao <pitzzahh@gmail.com>
2025-09-01 00:15:48 +0800

    feat: add overloaded GsisNotFoundException constructor for id and employeeId

--- src/main/java/dev/araopj/hrplatformapi/exception/GsisNotFoundException.java
+++ src/main/java/dev/araopj/hrplatformapi/exception/GsisNotFoundException.java
@@ -4,4 +4,8 @@ public class GsisNotFoundException extends IllegalArgumentException {
     public GsisNotFoundException(String id) {
         super("GSIS record with ID " + id + " not found.");
     }
+
+    public GsisNotFoundException(String id, String employeeId) {
+        super("Gsis record with id %s and employeeId %s not found.".formatted(id, employeeId));
+    }
 }
