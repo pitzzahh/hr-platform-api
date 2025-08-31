1bd645832da35deff88859b8965723d72809656e
Peter John Arao <pitzzahh@gmail.com>
2025-08-31 14:02:37 +0800

    feat: update SalaryDataResponse to include SalaryGrade object instead of salaryGradeId

--- src/main/java/dev/araopj/hrplatformapi/salary/dto/response/SalaryDataResponse.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/dto/response/SalaryDataResponse.java
@@ -1,10 +1,12 @@
 package dev.araopj.hrplatformapi.salary.dto.response;
 
+import dev.araopj.hrplatformapi.salary.model.SalaryGrade;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder
-public record SalaryDataResponse(String id, byte step, Double amount, String salaryGradeId, LocalDateTime createdAt,
+public record SalaryDataResponse(String id, byte step, Double amount, SalaryGrade salaryGrade, LocalDateTime createdAt,
                                  LocalDateTime updatedAt) {
 }
+
