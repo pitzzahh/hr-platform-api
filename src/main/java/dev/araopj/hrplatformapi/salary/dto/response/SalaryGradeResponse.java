946af6bab5e5b3fa949bef32f3fe22b250259feb
Peter John Arao <pitzzahh@gmail.com>
2025-08-31 13:59:49 +0800

    feat: add SalaryGradeResponse DTO for salary grade response structure

---
+++ src/main/java/dev/araopj/hrplatformapi/salary/dto/response/SalaryGradeResponse.java
@@ -0,0 +1,20 @@
+package dev.araopj.hrplatformapi.salary.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import dev.araopj.hrplatformapi.salary.model.SalaryData;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Builder
+public record SalaryGradeResponse(
+        String id,
+        String legalBasis,
+        byte tranche,
+        LocalDate effectiveDate,
+        byte salaryGrade,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        ArrayList<SalaryData> salaryData
+) {
+}
