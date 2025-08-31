c39f89980314c43322ccf2a60cb390e8fed3327e
Peter John Arao <pitzzahh@gmail.com>
2025-08-31 13:05:39 +0800

    feat: integrate dotenv-java for environment variable management and update Lombok dependency scope

--- src/main/java/dev/araopj/hrplatformapi/HrPlatformApiApplication.java
+++ src/main/java/dev/araopj/hrplatformapi/HrPlatformApiApplication.java
@@ -1,5 +1,6 @@
 package dev.araopj.hrplatformapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HrPlatformApiApplication {
 
     public static void main(String[] args) {
+        Dotenv.configure().directory("./").load().entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(HrPlatformApiApplication.class, args);
     }
 
