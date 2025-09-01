bff4fe78ce32e5edeabcdf1faf0897d22db2feaa
Peter John Arao <pitzzahh@gmail.com>
2025-09-01 17:22:58 +0800

    feat: add findByIdAndEmploymentInformation_Id method to DivisionStationPlaceOfAssignmentRepository

--- src/main/java/dev/araopj/hrplatformapi/employee/repository/DivisionStationPlaceOfAssignmentRepository.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/repository/DivisionStationPlaceOfAssignmentRepository.java
@@ -4,6 +4,9 @@ import dev.araopj.hrplatformapi.employee.model.DivisionStationPlaceOfAssignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DivisionStationPlaceOfAssignmentRepository extends JpaRepository<DivisionStationPlaceOfAssignment, String> {
+    Optional<DivisionStationPlaceOfAssignment> findByIdAndEmploymentInformation_Id(String id, String employeeId);
 }
