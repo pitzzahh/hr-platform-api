72a3de8d64e6b766ddd59e599c9b97f89d709b2a
Peter John Arao <pitzzahh@gmail.com>
2025-09-02 00:04:52 +0800

    fix: change IdentifierRepository to use String as the ID type

--- src/main/java/dev/araopj/hrplatformapi/employee/repository/IdentifierRepository.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/repository/IdentifierRepository.java
@@ -3,5 +3,5 @@ package dev.araopj.hrplatformapi.employee.repository;
 import dev.araopj.hrplatformapi.employee.model.Identifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IdentifierRepository extends JpaRepository<Identifier, Long> {
+public interface IdentifierRepository extends JpaRepository<Identifier, String> {
 }
