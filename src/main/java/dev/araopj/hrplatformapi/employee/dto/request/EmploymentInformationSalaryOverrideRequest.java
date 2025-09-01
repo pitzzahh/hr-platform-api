a40570d97bf2eabad32c1f4688e245085e59344e
Peter John Arao <pitzzahh@gmail.com>
2025-09-01 19:22:07 +0800

    refactor: convert EmploymentInformationSalaryOverrideRequest to a record and use Builder pattern

--- src/main/java/dev/araopj/hrplatformapi/employee/dto/request/EmploymentInformationSalaryOverrideRequest.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/dto/request/EmploymentInformationSalaryOverrideRequest.java
@@ -3,20 +3,18 @@ package dev.araopj.hrplatformapi.employee.dto.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.Builder;
 
 import java.time.LocalDate;
 
-@Data
-public class EmploymentInformationSalaryOverrideRequest {
-
-    @NotNull(message = "Salary cannot be null")
-    @Min(value = 1, message = "Salary must be at least 1")
-    private double salary;
-
-    @NotNull(message = "Effective date cannot be null")
-    private LocalDate effectiveDate;
-
-    @NotBlank(message = "Employment Information ID cannot be blank")
-    private String employmentInformationId;
+@Builder
+public record EmploymentInformationSalaryOverrideRequest(
+        @NotNull(message = "Salary cannot be null")
+        @Min(value = 1, message = "Salary must be at least 1")
+        double salary,
+        @NotNull(message = "Effective date cannot be null")
+        LocalDate effectiveDate,
+        @NotBlank(message = "Employment Information ID cannot be blank")
+        String employmentInformationId
+) {
 }
