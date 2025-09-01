0b56e9dcdf5c107bc268a93510958e149f380920
Peter John Arao <pitzzahh@gmail.com>
2025-09-01 13:57:01 +0800

    refactor: convert DivisionStationPlaceOfAssignmentRequest to a record and use Builder pattern

--- src/main/java/dev/araopj/hrplatformapi/employee/dto/request/DivisionStationPlaceOfAssignmentRequest.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/dto/request/DivisionStationPlaceOfAssignmentRequest.java
@@ -2,20 +2,17 @@ package dev.araopj.hrplatformapi.employee.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Null;
-import lombok.Data;
+import lombok.Builder;
 
-@Data
-public class DivisionStationPlaceOfAssignmentRequest {
-
-    @NotBlank(message = "Code cannot be blank")
-    private String code;
-
-    @NotBlank(message = "Name cannot be blank")
-    private String name;
-
-    @Null
-    private String shortName;
-
-    @NotBlank(message = "Employment Information ID cannot be blank")
-    private String employmentInformationId;
+@Builder
+public record DivisionStationPlaceOfAssignmentRequest(
+        @NotBlank(message = "Code cannot be blank")
+        String code,
+        @NotBlank(message = "Name cannot be blank")
+        String name,
+        @Null
+        String shortName,
+        @NotBlank(message = "Employment Information ID cannot be blank")
+        String employmentInformationId
+) {
 }
