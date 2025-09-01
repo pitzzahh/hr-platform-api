1443a20adfa33919b24b11ed9d8793d06158f9df
Peter John Arao <pitzzahh@gmail.com>
2025-09-02 00:59:20 +0800

    feat: add WITH_PARENT_REQUEST_PARAM to FetchType enum

--- src/main/java/dev/araopj/hrplatformapi/utils/enums/FetchType.java
+++ src/main/java/dev/araopj/hrplatformapi/utils/enums/FetchType.java
@@ -2,5 +2,6 @@ package dev.araopj.hrplatformapi.utils.enums;
 
 public enum FetchType {
     BY_PATH_VARIABLE,
-    WITH_PARENT_PATH_VARIABLE
+    WITH_PARENT_PATH_VARIABLE,
+    WITH_PARENT_REQUEST_PARAM,
 }
