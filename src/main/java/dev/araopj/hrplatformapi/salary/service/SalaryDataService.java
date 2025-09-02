204b960b11cecd7942eb85dfc962171ccd53afd9
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 01:01:21 +0800

    need to test soon

--- src/main/java/dev/araopj/hrplatformapi/salary/service/SalaryDataService.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/service/SalaryDataService.java
@@ -1,6 +1,5 @@
 package dev.araopj.hrplatformapi.salary.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.araopj.hrplatformapi.exception.NotFoundException;
 import dev.araopj.hrplatformapi.salary.dto.request.SalaryDataRequest;
 import dev.araopj.hrplatformapi.salary.dto.response.SalaryDataResponse;
@@ -8,28 +7,22 @@ import dev.araopj.hrplatformapi.salary.model.SalaryData;
 import dev.araopj.hrplatformapi.salary.repository.SalaryDataRepository;
 import dev.araopj.hrplatformapi.salary.repository.SalaryGradeRepository;
 import dev.araopj.hrplatformapi.utils.AuditUtil;
-import dev.araopj.hrplatformapi.utils.DiffUtil;
 import dev.araopj.hrplatformapi.utils.Mapper;
 import dev.araopj.hrplatformapi.utils.MergeUtil;
 import dev.araopj.hrplatformapi.utils.enums.CheckType;
-import dev.araopj.hrplatformapi.utils.enums.CreateType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
-import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 import static dev.araopj.hrplatformapi.audit.model.AuditAction.*;
 import static dev.araopj.hrplatformapi.exception.NotFoundException.EntityType.SALARY_DATA;
-import static dev.araopj.hrplatformapi.utils.DiffUtil.diff;
+import static dev.araopj.hrplatformapi.exception.NotFoundException.EntityType.SALARY_GRADE;
 import static dev.araopj.hrplatformapi.utils.JsonRedactor.redact;
-import static dev.araopj.hrplatformapi.utils.MergeUtil.merge;
 
 @RequiredArgsConstructor
 @Service
@@ -38,26 +31,24 @@ public class SalaryDataService {
 
     private final SalaryGradeRepository salaryGradeRepository;
     private final SalaryDataRepository salaryDataRepository;
-    private final ObjectMapper objectMapper;
     private final AuditUtil auditUtil;
     private final Set<String> REDACTED = Set.of("id", "salaryGrade");
     private final String ENTITY_NAME = "SalaryDataResponse";
 
-    public List<SalaryDataResponse> findAll(String salaryGradeId, Limit limit) {
-        var data = salaryDataRepository.findAll().stream()
-                .map(entity -> objectMapper.convertValue(entity, SalaryDataResponse.class))
-                .toList();
+    public List<SalaryDataResponse> findAll(String salaryGradeId, boolean withSalaryGrade, int limit) {
+
+        var pageable = PageRequest.of(0, limit);
+
+        var data = withSalaryGrade ?
+                salaryDataRepository.findAllWithParent(pageable)
+                : salaryDataRepository.findAll(pageable).getContent();
+
         if (salaryGradeId != null) {
             if (!salaryGradeRepository.existsById(salaryGradeId)) {
                 log.warn("SalaryGrade with id {} not found", salaryGradeId);
                 throw new NotFoundException(salaryGradeId, NotFoundException.EntityType.SALARY_GRADE);
             }
-            data = salaryDataRepository.findBySalaryGrade_Id(
-                            salaryGradeId,
-                            limit
-                    ).stream()
-                    .map(entity -> objectMapper.convertValue(entity, SalaryDataResponse.class))
-                    .toList();
+            data = salaryDataRepository.findBySalaryGradeId(salaryGradeId, pageable);
         }
 
         auditUtil.audit(
@@ -73,7 +64,9 @@ public class SalaryDataService {
                 ENTITY_NAME
         );
 
-        return data;
+        return data.stream()
+                .map(entity -> Mapper.toDto(entity, withSalaryGrade))
+                .toList();
     }
 
     public Optional<SalaryDataResponse> findById(String id) {
@@ -82,13 +75,13 @@ public class SalaryDataService {
                 ENTITY_NAME
         );
         return Optional.ofNullable(salaryDataRepository.findById(id)
-                .map(e -> objectMapper.convertValue(e, SalaryDataResponse.class))
+                .map(Mapper::toDto)
                 .orElseThrow(() -> new NotFoundException(id, SALARY_DATA)));
 
     }
 
     public Optional<SalaryDataResponse> findByIdAndSalaryGradeId(String id, String salaryGradeId) {
-        var data = salaryDataRepository.findByIdAndSalaryGrade_Id(id, salaryGradeId);
+        var data = salaryDataRepository.findByIdAndSalaryGradeId(id, salaryGradeId);
 
         if (data.isEmpty()) {
             log.warn("{} not found with id {} and salaryGradeId {}", ENTITY_NAME, id, salaryGradeId);
@@ -105,101 +98,117 @@ public class SalaryDataService {
         );
 
         return Optional.of(data
-                .map(e -> objectMapper.convertValue(e, SalaryDataResponse.class))
+                .map(Mapper::toDto)
                 .orElseThrow(() -> new NotFoundException(id, salaryGradeId, SALARY_DATA, "salaryGradeId")));
     }
 
     public Optional<SalaryDataResponse> create(
             SalaryDataRequest salaryDataRequest,
-            String salaryGradeId,
+            String salaryGradeId, // salary grade id from request param
             CheckType checkType
     ) throws BadRequestException {
-        var optionalSalaryGrade = switch (checkType) {
-            case CHECK_PARENT_FROM_REQUEST_PARAM -> salaryGradeRepository.findById(salaryGradeId);
-            case CHECK_PARENT_FROM_REQUEST_BODY -> salaryGradeRepository.findById(salaryDataRequest.salaryGradeId());
+        final var OPTIONAL_SALARY_GRADE_CHECK = switch (checkType) {
+            case CHECK_PARENT_FROM_REQUEST_PARAM -> {
+                if (salaryGradeId.isEmpty()) {
+                    throw new BadRequestException("Salary grade ID must be provided as query parameter when using CHECK_PARENT_FROM_REQUEST_PARAM.");
+                }
+                yield salaryGradeRepository.findById(salaryGradeId);
+            }
+            case CHECK_PARENT_FROM_REQUEST_BODY -> {
+                if (salaryDataRequest.salaryGradeId().isEmpty()) {
+                    throw new BadRequestException("Salary grade ID must be provided in request body when using CHECK_PARENT_FROM_REQUEST_BODY.");
+                }
+                yield salaryGradeRepository.findById(salaryDataRequest.salaryGradeId());
+            }
         };
 
-        if (optionalSalaryGrade.isPresent()) {
-            var sg = optionalSalaryGrade.get();
-            var existing_salary_data = salaryDataRepository.findByStepAndAmountAndSalaryGrade_Id(
-                    salaryDataRequest.step(),
-                    salaryDataRequest.amount(),
-                    sg.getId()
-            );
-            if (existing_salary_data.isPresent()) {
-                log.warn("SalaryData with step {} and amount {} already exists in SalaryGrade with id {}",
-                        salaryDataRequest.step(), salaryDataRequest.amount(), sg.getId());
-                throw new BadRequestException("SalaryData with the same step and amount already exists in the specified SalaryGrade.");
-            }
-        }
 
-        var data = Mapper.toDto(salaryDataRepository.saveAndFlush(
-                SalaryData.builder()
-                        .step(salaryDataRequest.step())
-                        .amount(salaryDataRequest.amount())
-                        .salaryGrade(optionalSalaryGrade.orElse(null))
-                        .build()
-        ));
+        final var SALARY_GRADE_ID_TO_CHECK = getId(salaryDataRequest, salaryGradeId, checkType);
+        final var RESOLVED_SALARY_GRADE = OPTIONAL_SALARY_GRADE_CHECK.orElseThrow(() -> new NotFoundException(SALARY_GRADE_ID_TO_CHECK, SALARY_GRADE));
+
+        salaryDataRepository.findByStepAndAmountAndSalaryGrade_Id(salaryDataRequest.step(), salaryDataRequest.amount(), SALARY_GRADE_ID_TO_CHECK)
+                .ifPresent(sd -> {
+                    throw new IllegalArgumentException(
+                            "Salary data with step %d amount %f, and salary grade %d already exists for salary grade ID [%s]".formatted(
+                                    salaryDataRequest.step(),
+                                    salaryDataRequest.amount(),
+                                    RESOLVED_SALARY_GRADE.getSalaryGrade(),
+                                    SALARY_GRADE_ID_TO_CHECK
+                            )
+                    );
+                });
+
+
+        final var SALARY_DATA_TO_SAVE = SalaryData.builder()
+                .step(salaryDataRequest.step())
+                .amount(salaryDataRequest.amount())
+                .salaryGrade(RESOLVED_SALARY_GRADE)
+                .build();
+
         auditUtil.audit(
                 CREATE,
-                String.valueOf(salaryDataRequest.step()),
+                String.valueOf(SALARY_DATA_TO_SAVE.getId()),
                 Optional.empty(),
-                redact(data, REDACTED),
+                redact(SALARY_DATA_TO_SAVE, REDACTED),
                 Optional.empty(),
                 ENTITY_NAME
         );
-        return Optional.of(data);
+        return Optional.of(Mapper.toDto(salaryDataRepository.saveAndFlush(SALARY_DATA_TO_SAVE)));
+
     }
 
     public SalaryDataResponse update(
             String id,
-            String salaryGradeId,
-            SalaryDataRequest salaryDataRequest,
-            CreateType createType,
-            boolean useWithParentId
-    ) {
+            SalaryDataRequest salaryDataRequest
+    ) throws BadRequestException {
 
-        var salaryDataResponse = switch (createType) {
-            case FROM_PATH_VARIABLE -> findById(id);
-            case FROM_REQUEST_BODY ->
-                    findByIdAndSalaryGradeId(id, useWithParentId ? salaryGradeId : salaryDataRequest.salaryGradeId());
-            default -> Optional.<SalaryDataResponse>empty();
-        };
-
-        if (salaryDataResponse.isEmpty()) {
-            log.warn("SalaryData with id {} and salary grade id {} not found", id, salaryGradeId);
-            throw new NotFoundException(
-                    id,
-                    salaryGradeId,
-                    SALARY_DATA,
-                    "salaryGradeId"
-            );
+        if (id == null || id.isEmpty()) {
+            throw new BadRequestException("SalaryData ID must be provided as path");
         }
 
-        var oldData = salaryDataResponse.get(); // Keep the old data for auditing
-        var newData = MergeUtil.merge(salaryDataResponse.get(), salaryDataRequest); // Merge old salaryDataResponse with new request salaryDataResponse
-        var changes = DiffUtil.diff(salaryDataResponse.get(), salaryDataRequest); // Compute the diff between old and new data
+        final var SALARY_GRADE_ID = salaryDataRequest.salaryGradeId();
 
-        if (changes.isEmpty()) {
-            log.info("No changes detected for salary data with id {}", id);
-            return oldData;
+        var SALARY_DATA = MergeUtil.merge(salaryDataRepository
+                        .findById(id).orElseThrow(() -> new NotFoundException(id, NotFoundException.EntityType.SALARY_DATA)),
+                Mapper.toEntity(salaryDataRequest)
+        );
+
+        if (SALARY_GRADE_ID != null && !SALARY_GRADE_ID.isEmpty()) {
+            final var SALARY_GRADE_PARENT = findByIdAndSalaryGradeId(id, SALARY_GRADE_ID)
+                    .orElseThrow(() -> new NotFoundException(id, SALARY_GRADE_ID, NotFoundException.EntityType.SALARY_DATA, "salaryGradeId"));
+            if (!Objects.equals(SALARY_GRADE_PARENT.id(), SALARY_DATA.getSalaryGrade().getId())) {
+                throw new BadRequestException("SalaryData with id %s does not belong to SalaryGrade with id %s".formatted(id, SALARY_GRADE_ID));
+            }
+
+            // check for existing entity based on new update values
+            salaryDataRepository.findByStepAndAmountAndSalaryGrade_Id(
+                    salaryDataRequest.step(),
+                    salaryDataRequest.amount(),
+                    SALARY_GRADE_ID
+            ).ifPresent(e -> {
+                if (!Objects.equals(e.getId(), id)) {
+                    throw new IllegalArgumentException(
+                            "Salary data with step %d amount %s, and salary grade %d already exists for salary grade ID [%s]".formatted(
+                                    salaryDataRequest.step(),
+                                    salaryDataRequest.amount() % 1 == 0 ? String.format("%.0f", salaryDataRequest.amount()) : String.format("%.2f", salaryDataRequest.amount()),
+                                    e.getSalaryGrade().getSalaryGrade(),
+                                    SALARY_GRADE_ID
+                            )
+                    );
+                }
+            });
         }
 
         auditUtil.audit(
                 UPDATE,
                 id,
-                Optional.of(redact(oldData, REDACTED)),
-                redact(merge(newData, salaryDataRequest), REDACTED),
-                Optional.of(redact(diff(oldData, salaryDataRequest), REDACTED)),
+                Optional.of(redact(SALARY_DATA, REDACTED)),
+                redact(MergeUtil.merge(SALARY_DATA, SALARY_DATA), REDACTED),
+                Optional.of(redact(SALARY_DATA, REDACTED)),
                 ENTITY_NAME
         );
 
-        return objectMapper.convertValue(
-                salaryDataRepository.saveAndFlush(
-                        objectMapper.convertValue(salaryDataResponse, SalaryData.class)
-                ),
-                SalaryDataResponse.class
-        );
+        return Mapper.toDto(salaryDataRepository.saveAndFlush(SALARY_DATA));
     }
 
     public boolean delete(String id, String salaryGradeId) {
@@ -222,5 +231,22 @@ public class SalaryDataService {
                 ENTITY_NAME
         );
         return true;
+    }
+
+    private static String getId(SalaryDataRequest request, String salaryGradeId, CheckType checkType) throws
+            BadRequestException {
+        switch (checkType) {
+            case CHECK_PARENT_FROM_REQUEST_BODY -> {
+                if (salaryGradeId == null || salaryGradeId.isEmpty())
+                    throw new BadRequestException("Salary grade ID must be provided in request body when using CHECK_PARENT_FROM_REQUEST_BODY.");
+                return request.salaryGradeId();
+            }
+            case CHECK_PARENT_FROM_REQUEST_PARAM -> {
+                if (salaryGradeId == null || salaryGradeId.isEmpty())
+                    throw new BadRequestException("Salary grade ID must be provided as query parameter when using CHECK_PARENT_FROM_REQUEST_PARAM.");
+                return salaryGradeId;
+            }
+            default -> throw new BadRequestException("Invalid CheckType provided.");
+        }
     }
 }
