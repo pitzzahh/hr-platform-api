83d94799dda0abbfd831a5dc151611ff62de992a
Peter John Arao <pitzzahh@gmail.com>
2025-09-02 14:00:46 +0800

    refactor: convert SalaryGradeRequest to a record and use Lombok's @Builder for improved structure

--- src/main/java/dev/araopj/hrplatformapi/salary/dto/request/SalaryGradeRequest.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/dto/request/SalaryGradeRequest.java
@@ -5,26 +5,24 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
-import lombok.Data;
+import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-@Data
-public class SalaryGradeRequest {
-    @NotNull(message = "Legal basis cannot be null")
-    private String legalBasis;
+@Builder
+public record SalaryGradeRequest(
+        @NotNull(message = "Legal basis cannot be null")
+        String legalBasis,
+        @Null
+        byte tranche,
+        @NotNull(message = "Effective date cannot be null")
+        LocalDate effectiveDate,
+        @Min(value = 1, message = "Salary grade must be at least 1")
+        @Max(value = 33, message = "Salary grade must be at most 33")
+        byte salaryGrade,
 
-    @Null
-    private byte tranche;
-
-    @NotNull(message = "Effective date cannot be null")
-    private LocalDate effectiveDate;
-
-    @Min(value = 1, message = "Salary grade must be at least 1")
-    @Max(value = 33, message = "Salary grade must be at most 33")
-    private byte salaryGrade;
-
-    @Null
-    private ArrayList<SalaryData> salaryData;
+        @Null
+        ArrayList<SalaryData> salaryData
+) {
 }
