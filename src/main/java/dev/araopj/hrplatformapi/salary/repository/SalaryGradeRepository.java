11ebd4a5f134c2cc6336bcf5e27545d6fc0b40ae
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 00:57:13 +0800

    refactor: remove commented-out query methods from SalaryGradeRepository

--- src/main/java/dev/araopj/hrplatformapi/salary/repository/SalaryGradeRepository.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/repository/SalaryGradeRepository.java
@@ -2,25 +2,22 @@ package dev.araopj.hrplatformapi.salary.repository;
 
 import dev.araopj.hrplatformapi.salary.model.SalaryGrade;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SalaryGradeRepository extends JpaRepository<SalaryGrade, String> {
-    @Query("SELECT sg FROM SalaryGrade sg LEFT JOIN FETCH sg.salaryData WHERE sg.id = :id")
+/*    @Query("SELECT sg FROM SalaryGrade sg LEFT JOIN FETCH sg.salaryData WHERE sg.id = :id")
     Optional<SalaryGrade> findByIdWithSalaryData(@Param("id") String id);
 
     @Query("SELECT DISTINCT sg FROM SalaryGrade sg LEFT JOIN FETCH sg.salaryData")
-    List<SalaryGrade> findAllWithSalaryData();
+    List<SalaryGrade> findAllWithSalaryData();*/
 
     Optional<SalaryGrade> findBySalaryGradeAndEffectiveDate(byte salaryGrade, LocalDate localDate);
-
+/*
     @Query("SELECT sg FROM SalaryGrade sg LEFT JOIN FETCH sg.salaryData WHERE sg.id = :id")
-    Optional<SalaryGrade> findSalaryGradeWithSalaryDataById(@Param("id") String id);
+    Optional<SalaryGrade> findSalaryGradeWithSalaryDataById(@Param("id") String id);*/
 
 }
