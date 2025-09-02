f1d0181cbdfda8c95dad2b5d13222ee5563a6257
Peter John Arao <pitzzahh@gmail.com>
2025-09-02 15:45:21 +0800

    refactor: add methods to SalaryGradeRepository for fetching salary grades with salary data

--- src/main/java/dev/araopj/hrplatformapi/salary/repository/SalaryGradeRepository.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/repository/SalaryGradeRepository.java
@@ -6,10 +6,21 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SalaryGradeRepository extends JpaRepository<SalaryGrade, String> {
     @Query("SELECT sg FROM SalaryGrade sg LEFT JOIN FETCH sg.salaryData WHERE sg.id = :id")
     Optional<SalaryGrade> findByIdWithSalaryData(@Param("id") String id);
+
+    @Query("SELECT DISTINCT sg FROM SalaryGrade sg LEFT JOIN FETCH sg.salaryData")
+    List<SalaryGrade> findAllWithSalaryData();
+
+    Optional<SalaryGrade> findBySalaryGradeAndEffectiveDate(byte salaryGrade, LocalDate localDate);
+
+    @Query("SELECT sg FROM SalaryGrade sg LEFT JOIN FETCH sg.salaryData WHERE sg.id = :id")
+    Optional<SalaryGrade> findSalaryGradeWithSalaryDataById(@Param("id") String id);
+
 }
