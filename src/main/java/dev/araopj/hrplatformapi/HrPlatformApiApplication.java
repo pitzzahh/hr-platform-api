f74b99a9e8edc551c41d9b0b84bacfc7c7c2e5f6
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 01:01:55 +0800

    probably redundant, but will check

--- src/main/java/dev/araopj/hrplatformapi/HrPlatformApiApplication.java
+++ src/main/java/dev/araopj/hrplatformapi/HrPlatformApiApplication.java
@@ -1,15 +1,24 @@
 package dev.araopj.hrplatformapi;
 
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HrPlatformApiApplication {
 
     public static void main(String[] args) {
         Dotenv.configure().directory("./").load().entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(HrPlatformApiApplication.class, args);
+    }
+
+    @Bean
+    public Hibernate6Module hibernate6Module() {
+        return new Hibernate6Module();
     }
 
 }
