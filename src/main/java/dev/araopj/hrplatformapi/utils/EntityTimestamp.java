ebac3b0069282e4242f766c2836eff454f542e74
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 01:02:07 +0800

    refactor: change timestamp fields from LocalDateTime to Instant

--- src/main/java/dev/araopj/hrplatformapi/utils/EntityTimestamp.java
+++ src/main/java/dev/araopj/hrplatformapi/utils/EntityTimestamp.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Getter
 @MappedSuperclass
@@ -14,10 +14,10 @@ public abstract class EntityTimestamp { // TODO: Tell spring to make this work
 
     @Column(nullable = false, updatable = false)
     @CreationTimestamp
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @Column(nullable = false)
     @UpdateTimestamp
-    private LocalDateTime updatedAt;
+    private Instant updatedAt;
 }
 
