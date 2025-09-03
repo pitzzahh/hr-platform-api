aaa5f02dbbb058ee495dbdfb7aaa4ea84a656bdb
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 13:22:55 +0800

    fix: replace SalaryDataResponse boxed Double amount to use primitive double

--- src/main/java/dev/araopj/hrplatformapi/salary/dto/response/SalaryDataResponse.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/dto/response/SalaryDataResponse.java
@@ -1,6 +1,5 @@
 package dev.araopj.hrplatformapi.salary.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dev.araopj.hrplatformapi.salary.model.SalaryGrade;
 import lombok.Builder;
@@ -11,7 +10,7 @@ import java.time.Instant;
 public record SalaryDataResponse(
         String id,
         byte step,
-        Double amount,
+        double amount,
         @JsonInclude(JsonInclude.Include.NON_NULL)
         SalaryGrade salaryGrade,
         Instant createdAt,
