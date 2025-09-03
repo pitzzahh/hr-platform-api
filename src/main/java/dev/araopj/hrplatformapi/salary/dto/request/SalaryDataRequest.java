ff8d9f74d5ae8a5055b900c70911a112194cf2e4
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 23:51:25 +0800

    refactor: enhance SalaryDataRequest validation with constraints for step and amount

--- src/main/java/dev/araopj/hrplatformapi/salary/dto/request/SalaryDataRequest.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/dto/request/SalaryDataRequest.java
@@ -1,19 +1,28 @@
 package dev.araopj.hrplatformapi.salary.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record SalaryDataRequest(
+        @Min(value = 1, message = "Salary step must be at least 1")
+        @Max(value = 8, message = "Salary step must be at most 8")
         byte step,
+        @NotNull(message = "Amount cannot be null")
         double amount,
         @NotBlank(message = "Salary grade ID is required")
         String salaryGradeId
-) implements ISalaryData {
+) {
     @Builder
     public record WithoutSalaryGradeId(
+            @Min(value = 1, message = "Salary step must be at least 1")
+            @Max(value = 8, message = "Salary step must be at most 8")
             byte step,
+            @NotNull(message = "Amount cannot be null")
             double amount
-    ) implements ISalaryData {
+    ) {
     }
 }
