194a1c1c203c3ef75dcc65b34092e16b2e1ea069
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 13:49:48 +0800

    feat: add validation for salaryGradeId in SalaryDataRequest

--- src/main/java/dev/araopj/hrplatformapi/salary/dto/request/SalaryDataRequest.java
+++ src/main/java/dev/araopj/hrplatformapi/salary/dto/request/SalaryDataRequest.java
@@ -1,11 +1,13 @@
 package dev.araopj.hrplatformapi.salary.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record SalaryDataRequest(
         byte step,
         double amount,
+        @NotBlank(message = "Salary grade ID is required")
         String salaryGradeId
 ) implements ISalaryData {
     @Builder
