eb08723a2ea29b62cb62b2327cf8c913f4301d32
Peter John Arao <pitzzahh@gmail.com>
2025-09-03 14:09:02 +0800

    feat: implement IDivisionStationPlaceOfAssignment interface and add support for optional employment information ID

--- src/main/java/dev/araopj/hrplatformapi/employee/dto/request/DivisionStationPlaceOfAssignmentRequest.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/dto/request/DivisionStationPlaceOfAssignmentRequest.java
@@ -1,18 +1,22 @@
 package dev.araopj.hrplatformapi.employee.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Null;
 import lombok.Builder;
 
 @Builder
 public record DivisionStationPlaceOfAssignmentRequest(
-        @NotBlank(message = "Code cannot be blank")
         String code,
-        @NotBlank(message = "Name cannot be blank")
         String name,
-        @Null
         String shortName,
         @NotBlank(message = "Employment Information ID cannot be blank")
         String employmentInformationId
-) {
+) implements IDivisionStationPlaceOfAssignment {
+
+    @Builder
+    public record WithoutEmploymentInformationId(
+            String code,
+            String name,
+            String shortName
+    ) implements IDivisionStationPlaceOfAssignment {
+    }
 }
