2eec8eeb77a0f87db2340fe683245497fbcf6884
Peter John Arao <pitzzahh@gmail.com>
2025-09-05 17:18:28 +0800

    feat: add JsonInclude annotation to EmploymentInformationResponse in PositionResponse

--- src/main/java/dev/araopj/hrplatformapi/employee/dto/response/PositionResponse.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/dto/response/PositionResponse.java
@@ -1,5 +1,6 @@
 package dev.araopj.hrplatformapi.employee.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
 import java.time.Instant;
@@ -9,6 +10,7 @@ public record PositionResponse(
         String id,
         String code,
         String description,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         EmploymentInformationResponse employmentInformationResponse,
         Instant createdAt,
         Instant updatedAt
