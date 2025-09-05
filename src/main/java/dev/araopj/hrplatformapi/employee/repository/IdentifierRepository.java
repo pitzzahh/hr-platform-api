6d2025dd6b2606a12cef7be6fa43120b2a2ea12b
Peter John Arao <pitzzahh@gmail.com>
2025-09-05 16:22:16 +0800

    refactor: add @Repository annotation to IdentifierRepository and remove NonNullApi from WorkplaceRepository

--- src/main/java/dev/araopj/hrplatformapi/employee/repository/IdentifierRepository.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/repository/IdentifierRepository.java
@@ -2,9 +2,11 @@ package dev.araopj.hrplatformapi.employee.repository;
 
 import dev.araopj.hrplatformapi.employee.model.Identifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IdentifierRepository extends JpaRepository<Identifier, String> {
     Optional<Identifier> findByIdentifierNumber(String s);
 }
--- src/main/java/dev/araopj/hrplatformapi/employee/repository/WorkplaceRepository.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/repository/WorkplaceRepository.java
@@ -1,13 +1,11 @@
 package dev.araopj.hrplatformapi.employee.repository;
 
 import dev.araopj.hrplatformapi.employee.model.Workplace;
-import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@NonNullApi
 @Repository
 public interface WorkplaceRepository extends JpaRepository<Workplace, String> {
     Optional<Workplace> findByCodeAndNameAndEmploymentInformationId(String code, String name, String employmentInformationId);
