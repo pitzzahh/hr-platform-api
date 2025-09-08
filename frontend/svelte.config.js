f37c56d8f2a173ff51b0ad53cf3a01add7c4768e
Peter John Arao <pitzzahh@gmail.com>
2025-09-09 02:30:16 +0800

    feat: configure SvelteKit for embedded output with inline bundle strategy

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,15 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
+        embedded: true,
+        output: {
+            bundleStrategy: "inline",
+            assetsInlineLimit: Infinity
+        },
         adapter: adapter({
             pages: '../src/main/resources/static',
             assets: '../src/main/resources/static',
-            fallback: null
+            fallback: null,
         })
-    }
+    },
 };
 
 export default config;
