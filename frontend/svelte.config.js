2cdd5e615e6e1f4ef248ee0b9d53f059ca04e50d
Peter John Arao <pitzzahh@gmail.com>
2025-09-09 02:31:56 +0800

    fix: update SvelteKit configuration to inline all imported assets

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -4,11 +4,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
+    build: {
+        // inline all imported assets
+        assetsInlineLimit: Infinity
+    },
     kit: {
         embedded: true,
         output: {
-            bundleStrategy: "inline",
-            assetsInlineLimit: Infinity
+            bundleStrategy: "inline"
         },
         adapter: adapter({
             pages: '../src/main/resources/static',
