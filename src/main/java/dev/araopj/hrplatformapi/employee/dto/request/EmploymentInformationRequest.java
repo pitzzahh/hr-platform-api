3323f26d2f98e3d2786cf99fdde9e4cde78a7c68
Peter John Arao <pitzzahh@gmail.com>
2025-09-17 10:24:02 +0800

    refactor: simplify validation annotations in EmployeeRequest and EmploymentInformationRequest

--- src/main/java/dev/araopj/hrplatformapi/employee/dto/request/EmploymentInformationRequest.java
+++ src/main/java/dev/araopj/hrplatformapi/employee/dto/request/EmploymentInformationRequest.java
@@ -1,37 +1,36 @@
 package dev.araopj.hrplatformapi.employee.dto.request;
 
 import dev.araopj.hrplatformapi.employee.model.EmploymentStatus;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.time.LocalDate;
 
 @Builder
 public record EmploymentInformationRequest(
-        @NotNull
-        @NotBlank
+        @NotNull(message = "employeeId cannot be null")
+        @NotBlank(message = "employeeId cannot be blank")
         String employeeId,
-        @NotNull
+        @NotNull(message = "startDate cannot be null")
         LocalDate startDate,
         LocalDate endDate,
-        @NotNull
+        @NotNull(message = "employmentStatus cannot be null")
         EmploymentStatus employmentStatus,
-        @NotNull
-        @NotBlank
+        @NotNull(message = "sourceOfFund cannot be null")
+        @NotBlank(message = "sourceOfFund cannot be blank")
         String sourceOfFund,
         String remarks,
-        EmploymentInformationSalaryOverrideRequest employmentInformationSalaryOverrideRequest,
-        @Size(min = 1, max = 8, message = "step must be between 1 and 8")
-        int step,
-        @Size(min = 1, max = 8, message = "anticipatedStep must be between 1 and 8")
-        int anticipatedStep,
-        @NotNull
-        @NotBlank
+        String employmentInformationSalaryOverrideId,
+        @NotNull(message = "positionId cannot be null")
+        @NotBlank(message = "positionId cannot be blank")
         String positionId,
-        @NotNull
-        @NotBlank
-        String workplaceId
+        @NotNull(message = "workplaceId cannot be null")
+        @NotBlank(message = "workplaceId cannot be blank")
+        String workplaceId,
+        @NotNull(message = "salaryId cannot be null")
+        @NotBlank(message = "salaryId cannot be blank")
+        String salaryId
 ) {
 }
