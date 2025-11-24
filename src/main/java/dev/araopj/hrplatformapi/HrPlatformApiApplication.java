78a22ed4b4c15def6deb990dc8550d7bd8b40e7a
pitzzahh <pitzzahh@gmail.com>
2025-11-24 17:32:34 +0800

    add: dotenv config

--- src/main/java/dev/araopj/hrplatformapi/HrPlatformApiApplication.java
+++ src/main/java/dev/araopj/hrplatformapi/HrPlatformApiApplication.java
@@ -1,7 +1,9 @@
 package dev.araopj.hrplatformapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,4 +14,8 @@ public class HrPlatformApiApplication {
         SpringApplication.run(HrPlatformApiApplication.class, args);
     }
 
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.load();
+    }
 }
--- src/main/java/dev/araopj/hrplatformapi/config/WebConfig.java
+++ src/main/java/dev/araopj/hrplatformapi/config/WebConfig.java
@@ -1,21 +1,25 @@
 package dev.araopj.hrplatformapi.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final Environment env;
+    private final Dotenv env;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins(env.getProperty("CORS_ALLOWED_ORIGINS", "http://localhost:5173").split(","))
+                .allowedOrigins(env.get("CORS_ALLOWED_ORIGINS", "http://localhost:5173").split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
